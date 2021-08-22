6028186d98f11857b74238a823812d0ca543b869
clevercoderjoy <74505057+clevercoderjoy@users.noreply.github.com>
2021-08-22 10:13:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
-// Ex:1
+// // Ex:1
 
-// console.log("Joy")
+// // console.log("Joy")
 
-// Ex: 2
+// // Ex: 2
 
 var readlineSync = require('readline-sync')
-var name = readlineSync.question("What is your name?\n")
-// console.log(name)
+// var name = readlineSync.question("What is your name?\n")
+// // console.log(name)
 
-// Ex: 3
+// // Ex: 3
 
-var welcomeMessage = "Hi! " + name + ", Welcome to JavaScript!"
-console.log(welcomeMessage)
+// var welcomeMessage = "Hi! " + name + ", Welcome to JavaScript!"
+// console.log(welcomeMessage)
 
-// Ex: 4
-// Ex: 5
-// Ex: 6
+// // Ex: 4
+// // Ex: 5
+// // Ex: 6
 
-var score = 0
-var age = readlineSync.question("Enter your age to check if it is greater than 18 or not.\n")
-if (age >= 18){
-    console.log("Yes! You are Correct. Your age is " + age + " which is greater than 18.\n")
-    score = score + 1
-}
-else{
-    console.log("No! You are wrong. Your age is " + age + " which is less than 18.\n")
-    score = score - 1
-}
+// var score = 0
+// var age = readlineSync.question("Enter your age to check if it is greater than 18 or not.\n")
+// if (age >= 18){
+//     console.log("Yes! You are Correct. Your age is " + age + " which is greater than 18.\n")
+//     score = score + 1
+// }
+// else{
+//     console.log("No! You are wrong. Your age is " + age + " which is less than 18.\n")
+//     score = score - 1
+// }
+
+// console.log("The score is " + score)
+
+// var newQues = readlineSync.question("Do you have an imposter syndrome?\n")
+// if (newQues === 'yes'){
+//     score = score + 1
+//     console.log("Great!!! So you are a pro coder.\nYour score now is: " + score + ".\n")
+// }
+// else if (newQues === 'no'){
+//     score = score - 1
+//     console.log("Ohh!!! So you are a noob.\nYour score now is: " + score + ".\n")
+// }
+// else{
+//     score = 0
+//     console.log("Invalid input\n Your score has been reset to " + score+ ".\n")
+// }
 
 // Ex: 7
+// functions
 
-console.log("The score is " + score)
+// Syntax
+// function functionName(par1, par2){
+    // processing
+//     return output
+// }
 
-// Ex: 8
+function add(num1, num2){
+    var sum = num1 + num2
+    return sum
+}
 
-var newQues = readlineSync.question("Do you have an imposter syndrome?\n")
-if (newQues === 'yes'){
-    score = score + 1
-    console.log("Great!!! So you are a pro coder.\nYour score now is: " + score + ".\n")
-}
-else if (newQues === 'no'){
-    score = score - 1
-    console.log("Ohh!!! So you are a noob.\nYour score now is: " + score + ".\n")
-}
-else{
-    score = 0
-    console.log("Invalid input\n Your score has been reset to " + score+ ".\n")
-}
+console.log("Enter two numbers to add them.\n")
+var num1 = Number(readlineSync.question())
+var num2 = Number(readlineSync.question())
+var result = add(num1, num2)
+console.log(result)
